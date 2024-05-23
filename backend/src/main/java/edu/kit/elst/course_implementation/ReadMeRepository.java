ca3bb66482993d797ab639fa3e316ac59288c342
Daniel Augustin <daniel.augustin1@gmx.de>
2024-05-23 17:46:25 +0200

    Remove old building block implementation module

--- backend/src/main/java/edu/kit/elst/course_implementation/ReadMeRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.kit.elst.course_implementation;
-
-import edu.kit.elst.building_blocks.BuildingBlockVersion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReadMeRepository extends JpaRepository<ReadMe, BuildingBlockVersion> {
-}
